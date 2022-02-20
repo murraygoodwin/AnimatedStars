0e6a6bb69184c372e576a9e03c5ca427efdf5ca7
Murray Goodwin <murray.goodwin.uk@gmail.com>
2022-02-20 16:25:11 +0000

    Add bubbles option

--- Sources/AnimatedStars/AnimatedStars.swift
+++ Sources/AnimatedStars/AnimatedStars.swift
@@ -2,16 +2,24 @@ import UIKit
 
 public enum AnimatedStars {
   
-  public static func risingStars(viewController: UIViewController) {
-    animate(
-      viewController: viewController,
-      numberOfImages: 50,
-      imageSizeMinimum: 20,
-      imageSizeMaximum: 50,
-      riseTimeMin: 0.8,
-      riseTimeMax: 3.0,
-      image: UIImage(systemName: "star.fill"),
-      imageTintColor: .yellow)
+  public enum Animation {
+    case risingStars
+    case bubbles
+  }
+  
+  public static func animate(animation: Animation, viewController: UIViewController) {
+    switch animation {
+      case .risingStars:    risingStars(viewController: viewController)
+      case .bubbles:        bubbles(viewController: viewController)
+    }
+  }
+  
+  private static func risingStars(viewController: UIViewController) {
+    animate(viewController: viewController, numberOfImages: 50, imageSizeMinimum: 20, imageSizeMaximum: 50, riseTimeMin: 0.8, riseTimeMax: 3.0,  image: UIImage(systemName: "star.fill"), imageTintColor: .yellow)
+  }
+  
+  private static func bubbles(viewController: UIViewController) {
+    animate(viewController: viewController, numberOfImages: 25, imageSizeMinimum: 8, imageSizeMaximum: 35, riseTimeMin: 0.8, riseTimeMax: 3.0,  image: UIImage(systemName: "circle.fill"), imageTintColor: .yellow)
   }
   
   private static func animate(viewController: UIViewController, numberOfImages: Int, imageSizeMinimum: Int, imageSizeMaximum: Int, riseTimeMin: Double, riseTimeMax: Double, image: UIImage?, imageTintColor: UIColor) {
